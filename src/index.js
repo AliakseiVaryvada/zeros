ff90350deba3fba8d2c4343a1634a18eb71287ba
Alivarik <45916819+Alivarik@users.noreply.github.com>
2019-10-05 22:05:40 +0300

    index.js: edit solution

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ function factorial(n) {
 }
 
 function factorialCh(n) {
-    return n != 1 ? n * factorialCh(n - 2) : 1;
+    return n >= 1 ? n * factorialCh(n - 2) : 1;
 }
 
 function zeros(expression) {
-    let n = 0;
+    let m = 0;
     let numb = 0;
     let itog = 1;
     let mass = expression.split("*");
@@ -16,15 +16,17 @@ function zeros(expression) {
     let zero = 0;
     mass.forEach(element => {
         console.log(element);
-        n = element.match(/\!/g || []).length;
-        console.log(n);
-        numb = +element.substring(0, element.length - n);
-        if (n === 1) {
+        m = element.match(/\!/g || []).length;
+        console.log(m);
+        numb = +element.substring(0, element.length - m);
+        if (m === 1) {
             fac = factorial(numb);
         } else {
             fac = factorialCh(numb);
         }
         itog = fac * itog;
+        fac = 0;
+        console.log(itog);
     });
     console.log(itog);
 
