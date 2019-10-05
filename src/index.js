543d01b11b9a38cbdefcd588851734ba63dad987
Alivarik <45916819+Alivarik@users.noreply.github.com>
2019-10-05 18:42:29 +0300

    index.js: first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function zeros(expression) {
-  // your solution
+function factorial(n) {
+    return n != 1 ? n * factorial(n - 1) : 1;
 }
+
+function factorialCh(n) {
+    return n != 1 ? n * factorialCh(n - 2) : 1;
+}
+
+function zeros(expression) {
+    let n = 0;
+    let numb = 0;
+    let itog = 1;
+    let mass = expression.split("*");
+    let fac = 1;
+    let str = "";
+    let zero = 0;
+    mass.forEach(element => {
+        console.log(element);
+        n = element.match(/\!/g || []).length;
+        console.log(n);
+        numb = +element.substring(0, element.length - n);
+        if (n === 1) {
+            fac = factorial(numb);
+        } else {
+            fac = factorialCh(numb);
+        }
+        itog = fac * itog;
+    });
+    console.log(itog);
+
+    return itog;
+}
+console.log(zeros("9!!*10!!*7!!"));
